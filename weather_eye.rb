e04b1ed922eb8febda874b9715808f86590f4204
Tristan Harris <tris.dev@pumpcottage.me.uk>
2010-12-12 18:39:21 +0000

    Upload unsent samples

--- weather_eye.rb
+++ weather_eye.rb
@@ -4,20 +4,26 @@ $:.unshift File.join(File.dirname(__FILE__), 'lib')
 require 'weather_eye'
 require "net/http"
 require 'digest'
+require 'json'
+require 'time'
 
  SECRET = ENV['WEATHER_SECRET'] || 'you need to set this'
 
 w = WeatherEye.new
 data = w.current
-puts data.to_h.inspect
-params = {}
-data.to_h.each_pair{|k, v| params["data[#{k}]"] = v}
-params[:md5] = Digest::MD5.hexdigest(data.md5 + SECRET)
-x = Net::HTTP.post_form(URI.parse('http://localhost:9393/data'), params)
-puts x.body
-#w.back_to(Time.now - 50*60) do |sample|
-#  puts sample.inspect
-#end
+
+x = Net::HTTP.get(URI.parse('http://localhost:9393/last'))
+start_time = Time.parse(JSON.parse(x)['latest_record'])
+
+w.back_to(start_time) do |sample|
+  puts '-----'+sample.inspect
+  data=sample
+  params = {}
+  data.to_h.each_pair{|k, v| params["data[#{k}]"] = v}
+  params[:md5] = Digest::MD5.hexdigest(data.md5 + SECRET)
+  x = Net::HTTP.post_form(URI.parse('http://localhost:9393/data'), params)
+  puts x.body
+end
 
 #w.each do |sample|
 #  puts sample.inspect
