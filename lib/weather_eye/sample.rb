e4691b3880c568ee9cb3f3905f191bfe378bcf61
Tristan Harris <tris.dev@pumpcottage.me.uk>
2010-12-12 17:50:52 +0000

    Rename sample fields & add md5

--- lib/weather_eye/sample.rb
+++ lib/weather_eye/sample.rb
@@ -1,7 +1,8 @@
   class WeatherEye::Sample
-    ATTRS = [:time, :offset, :humidity_in, :temp_in, :humidity_out, :temp_out, :pressure,
-             :wind_speed, :gust_speed, :direction, :rain]
+    ATTRS = [:time, :indoor_humidity, :indoor_temperature, :outdoor_humidity,
+              :outdoor_temperature, :pressure, :wind_speed, :gust_speed, :wind_direction, :rainfall]
     attr_reader *ATTRS
+    attr_reader :offset
 
     WIND_TRANDUCER_FACTOR = 0.38
     RAIN_TRANDUCER_FACTOR = 0.3
@@ -13,37 +14,37 @@
       data = {}
       data[:time] = time
       data[:offset] = raw[0] # minutes after last sample
-      data[:humidity_in] = raw[1]
-      data[:temp_in] = (raw[3] << 8) + raw[2]
-      data[:humidity_out] = raw[4]
-      data[:temp_out] = (raw[6] << 8) + raw[5]
+      data[:indoor_humidity] = raw[1]
+      data[:indoor_temperature] = (raw[3] << 8) + raw[2]
+      data[:outdoor_humidity] = raw[4]
+      data[:outdoor_temperature] = (raw[6] << 8) + raw[5]
       data[:pressure] = (raw[8] << 8) + raw[7]
       data[:wind_speed] = ((raw[11] & 0x0f) << 8) + raw[9]
       data[:gust_speed] = ((raw[11] & 0xf0) << 4) + raw[10]
-      data[:direction] = raw[12]
-      data[:rain] = ((raw[14] << 8) + raw[13])
+      data[:wind_direction] = raw[12]
+      data[:rainfall] = ((raw[14] << 8) + raw[13])
       status = raw[15]
 
-      data[:humidity_in] = nil if data[:humidity_in] == 0xff
-      data[:temp_in] = nil if data[:temp_in] == 0xffff
-      data[:humidity_out] = nil if data[:humidity_out] == 0xff
-      data[:temp_out] = nil if data[:temp_out] == 0xffff
+      data[:indoor_humidity] = nil if data[:indoor_humidity] == 0xff
+      data[:indoor_temperature] = nil if data[:indoor_temperature] == 0xffff
+      data[:outdoor_humidity] = nil if data[:outdoor_humidity] == 0xff
+      data[:outdoor_temperature] = nil if data[:outdoor_temperature] == 0xffff
       data[:pressure] = nil if data[:pressure] == 0xffff
       data[:wind_speed] = nil if raw[9] == 0xff
       data[:gust_speed] = nil if raw[10] == 0xff
-      data[:direction] = nil if data[:direction][7] > 0
-      data[:rain] = nil if status[6] > 0
+      data[:wind_direction] = nil if data[:wind_direction][7] > 0
+      data[:rainfall] = nil if status[6] > 0
       raise 'rain overflow' if status[7] > 0
 
-      data[:temp_in] = -(data[:temp_in] & 0x7fff) if data[:temp_in][15] > 0
-      data[:temp_out] = -(data[:temp_out] & 0x7fff) if data[:temp_out][15] > 0
+      data[:indoor_temperature] = -(data[:indoor_temperature] & 0x7fff) if data[:indoor_temperature][15] > 0
+      data[:outdoor_temperature] = -(data[:outdoor_temperature] & 0x7fff) if data[:outdoor_temperature][15] > 0
 
-      data[:temp_in] /= TEMP_FACTOR
-      data[:temp_out] /= TEMP_FACTOR
+      data[:indoor_temperature] /= TEMP_FACTOR
+      data[:outdoor_temperature] /= TEMP_FACTOR
       data[:pressure] /= PRESSURE_FACTOR
       data[:wind_speed] *= WIND_TRANDUCER_FACTOR
       data[:gust_speed] *= WIND_TRANDUCER_FACTOR
-      data[:rain] *= RAIN_TRANDUCER_FACTOR
+      data[:rainfall] *= RAIN_TRANDUCER_FACTOR
 
       self.new(data)
     end
@@ -52,10 +53,19 @@
       ATTRS.each do |a|
         instance_variable_set "@#{a}", data[a]
       end
+      offset = data[:offset]
     end
 
     def to_h
       ATTRS.inject({}){|h, a| h[a] = instance_variable_get("@#{a}");h}
+    end
+
+    def md5
+      Digest::MD5.hexdigest(data = ATTRS.map do |a|
+        v = instance_variable_get("@#{a}")
+        v = v.to_i if (v.is_a? Time)
+        v.to_f.to_s
+      end.join)
     end
 
   end
--- weather_eye.rb
+++ weather_eye.rb
@@ -2,10 +2,22 @@
 
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
 require 'weather_eye'
+require "net/http"
+require 'digest'
+
+ SECRET = ENV['WEATHER_SECRET'] || 'you need to set this'
 
 w = WeatherEye.new
-puts w.current.inspect
-puts
+data = w.current
+puts data.to_h.inspect
+params = {}
+data.to_h.each_pair{|k, v| params["data[#{k}]"] = v}
+params[:md5] = Digest::MD5.hexdigest(data.md5 + SECRET)
+x = Net::HTTP.post_form(URI.parse('http://localhost:9393/data'), params)
+puts x.body
+#w.back_to(Time.now - 50*60) do |sample|
+#  puts sample.inspect
+#end
 
 #w.each do |sample|
 #  puts sample.inspect
